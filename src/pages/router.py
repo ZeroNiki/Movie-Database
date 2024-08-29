366f915b1c1991051410a6bf1c22d9795b8dcf1a
Nikita Menshov <nikitatop2222@gmail.com>
2024-08-29 16:14:15 +0500

    displaying movies on the home page

--- src/pages/router.py
+++ src/pages/router.py
@@ -12,4 +12,20 @@ templates = Jinja2Templates(directory="src/templates")
 
 @router.get("/home")
 def home_page(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    links = [
+        {'url': 'https://image.tmdb.org/t/p/w500/8cdWjvZQUExUUTzyp4t6EDMubfO.jpg',
+            'name': "Deadpool & Wolverine", "rating": round(7.77)},
+        {'url': 'https://image.tmdb.org/t/p/w500/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg',
+            'name': "Inside Out 2", "rating": round(7.685)},
+        {'url': 'https://image.tmdb.org/t/p/w500/wWba3TaojhK7NdycRhoQpsG0FaH.jpg',
+            'name': "Despicable Me 4", "rating": round(7.3)},
+        {'url': 'https://image.tmdb.org/t/p/w500/pjnD08FlMAIXsfOLKQbvmO0f0MD.jpg',
+            'name': "Twisters", "rating": round(7.046)},
+        {'url': 'https://image.tmdb.org/t/p/w500/d9CTnTHip1RbVi2OQbA2LJJQAGI.jpg',
+            'name': "The Union", "rating": round(6.347)},
+    ]
+
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request, "links": links}
+    )
