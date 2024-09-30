5b3d952b9748f7afbfacc53720e8654120a2a591
Nikita Menshov <nikitatop2222@gmail.com>
2024-09-30 15:58:14 +0500

    Update test_operations.py

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -4,9 +4,3 @@ async def test_get_movie(ac: AsyncClient):
     response = await ac.get("/operations/data")
 
     assert response.status_code == 200
-
-
-async def test_get_movie(ac: AsyncClient):
-    response = await ac.get("/operations/data")
-
-    assert response.status_code == 200
